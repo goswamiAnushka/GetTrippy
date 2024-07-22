9d5bcb2240a894ef9ee2b8c2d46250f0648147ef
Anushka Goswami <139368085+goswamiAnushka@users.noreply.github.com>
2024-07-22 18:25:56 +0530

    updating src

--- JourneyGenie/src/main.jsx
+++ JourneyGenie/src/main.jsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import CreateTrip from './create-trip/index.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router=createBrowserRouter([
   {
     path:'/',
     element:<App/>
+  },
+  {
+    path:'/create-trip',
+    element:<CreateTrip/>
   }
 ])
 ReactDOM.createRoot(document.getElementById('root')).render(
